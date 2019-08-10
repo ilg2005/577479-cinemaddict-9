8603f1309bea0d83e6aa9bee9165685e0fd998f9
ilg2005 <igoldfarb10@gmail.com>
2019-08-10 11:32:07 +0300

    Модуль с разметкой звания пользователя создан и отрисован

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import {getSearchMarkup} from "./components/search";
+import {getUserProfileMarkup} from "./components/user-profile";
+
 
 const renderElement = (element, markup, renderingCount = 1) => {
   for (let i = 1; i <= renderingCount; i++) {
@@ -9,3 +11,4 @@ const renderElement = (element, markup, renderingCount = 1) => {
 const headerElement = document.querySelector(`.header`);
 
 renderElement(headerElement, getSearchMarkup());
+renderElement(headerElement, getUserProfileMarkup());
