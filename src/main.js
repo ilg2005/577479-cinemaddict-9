0d5f459ddb9fcc634aace37127de6e43fd55d184
ilg2005 <igoldfarb10@gmail.com>
2019-08-10 12:29:56 +0300

    Модуль с разметкой контейнера контента создан и отрисован

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {getSearchMarkup} from "./components/search";
 import {getUserProfileMarkup} from "./components/user-profile";
 import {getMenuMarkup} from "./components/menu";
 import {getSortMarkup} from "./components/sort";
+import {getContentContainerMarkup} from "./components/content-container";
 
 
 const renderElement = (element, markup, renderingCount = 1) => {
@@ -17,3 +18,4 @@ renderElement(headerElement, getSearchMarkup());
 renderElement(headerElement, getUserProfileMarkup());
 renderElement(mainElement, getMenuMarkup());
 renderElement(mainElement, getSortMarkup());
+renderElement(mainElement, getContentContainerMarkup());
