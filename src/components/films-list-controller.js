a7926f43bd33fae46ec2e5f923cf83f8b8e6c1ef
ilg2005 <igoldfarb10@gmail.com>
2019-09-08 13:11:53 +0300

    изменена логика отрисовки фильмов в контроллере

--- src/components/films-list-controller.js
+++ src/components/films-list-controller.js
@@ -5,51 +5,28 @@ import ShowMoreBtn from "./show-more-btn.js";
 import NoMovies from "./no-movies.js";
 
 export default class FilmsListController {
-  constructor(filmsListElement, films) {
-    this._filmsListElement = filmsListElement;
-    this._allMoviesContainer = this._filmsListElement.querySelector(`.all-movies`);
+  constructor(container, films) {
+    this._filmsListElement = container;
+    this._allMoviesContainer = container.querySelector(`.all-movies`);
     this._films = films;
+    this.FILMS_PORTION_TO_RENDER = 5;
+    this._restFilms = null;
     this._sort = new Sort();
     this._showMoreBtn = new ShowMoreBtn();
   }
 
-  _renderFilms(container, films) {
+  _renderFilmsPortion(filmsArray) {
     const renderFilm = (filmCard) => {
       const film = new Film(filmCard);
-
-      utils.render(container, film.getElement(), `beforeend`);
+      utils.render(this._allMoviesContainer, film.getElement(), `beforeend`);
       film.getElement().id = filmCard.id;
     };
-
-    const filmsLoaderElement = document.querySelector(`.films-list__show-more`);
-
-    const renderFilmsPortion = (filmsArray, initialFilmsArrayLength) => {
-      let length = filmsArray.length > FILMS_PORTION_TO_RENDER ? FILMS_PORTION_TO_RENDER : filmsArray.length;
-      for (let i = 0; i < length; i++) {
-        renderFilm(filmsArray[i]);
-      }
-      const renderedFilmsCount = container.querySelectorAll(`.film-card`).length;
-      if (renderedFilmsCount === initialFilmsArrayLength) {
-        filmsLoaderElement.classList.add(`hide`);
-        filmsLoaderElement.removeEventListener(`click`, filmsLoaderElementClickHandler);
-      } else {
-        filmsLoaderElement.classList.remove(`hide`);
-      }
-    };
-
-    const filmsLoaderElementClickHandler = () => {
-      let filmsCopy = films.slice();
-      const renderedFilmsCount = container.querySelectorAll(`.film-card`).length;
-      filmsCopy.splice(0, renderedFilmsCount);
-      renderFilmsPortion(filmsCopy, films.length);
-    };
-
-    filmsLoaderElement.addEventListener(`click`, filmsLoaderElementClickHandler);
-
-    const FILMS_PORTION_TO_RENDER = 5;
-
-
-    renderFilmsPortion(films, films.length);
+    const filmsArrayClone = filmsArray.slice();
+    const filmsToRender = filmsArrayClone.splice(0, this.FILMS_PORTION_TO_RENDER);
+    for (let film of filmsToRender) {
+      renderFilm(film);
+    }
+    return filmsArrayClone;
   }
 
   init() {
@@ -57,11 +34,25 @@ export default class FilmsListController {
       const noMovies = new NoMovies();
       utils.render(this._allMoviesContainer, noMovies.getElement(), `beforeend`);
     } else {
-      utils.render(this._allMoviesContainer.parentElement, this._showMoreBtn.getElement(), `beforeend`);
-      this._renderFilms(this._allMoviesContainer, this._films);
-      const sortElement = this._sort.getElement();
-      utils.render(this._allMoviesContainer.parentElement, sortElement, `afterbegin`);
+      this._restFilms = this._renderFilmsPortion(this._films);
 
+      if (this._restFilms.length) {
+        const showMoreBtnElement = this._showMoreBtn.getElement();
+        utils.render(this._filmsListElement, showMoreBtnElement, `beforeend`);
+        const showMoreBtnElementClickHandler = () => {
+          this._restFilms = this._renderFilmsPortion(this._restFilms);
+          if (!this._restFilms.length) {
+            showMoreBtnElement.removeEventListener(`click`, showMoreBtnElementClickHandler);
+            utils.unrender(showMoreBtnElement);
+          }
+        };
+        showMoreBtnElement.addEventListener(`click`, showMoreBtnElementClickHandler);
+      }
+
+
+      const sortElement = this._sort.getElement();
+      utils.render(this._filmsListElement, sortElement, `afterbegin`);
+      /*
       const sortElementClickHandler = (evt) => {
         evt.preventDefault();
         if (evt.target.tagName !== `A`) {
@@ -75,21 +66,22 @@ export default class FilmsListController {
           case `date`:
             evt.target.classList.add(`sort__button--active`);
             const filmsByDate = this._films.slice().sort((a, b) => b.dateInTimestamp - a.dateInTimestamp);
-            this._renderFilms(this._allMoviesContainer, filmsByDate);
+            this._renderFilmsPortion(filmsByDate);
             break;
           case `rating`:
             evt.target.classList.add(`sort__button--active`);
             const filmsByRating = this._films.slice().sort((a, b) => b.rating - a.rating);
-            this._renderFilms(this._allMoviesContainer, filmsByRating);
+            this._renderFilmsPortion(filmsByRating);
             break;
           case `default`:
             evt.target.classList.add(`sort__button--active`);
-            this._renderFilms(this._allMoviesContainer, this._films);
+            this._renderFilmsPortion(this._films);
             break;
         }
       };
 
       sortElement.addEventListener(`click`, sortElementClickHandler);
+*/
     }
   }
 }
